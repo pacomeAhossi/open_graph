fa07e2542bc3ef89a097918f0c2ac1fcbc8271a3
Pacome <a.pacome@lanignan.com>
2023-03-21 18:20:38 +0100

    traitement du bouton qui permet de copier

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 
     let baseUrlLinkedin = 'https://www.linkedin.com/posts/lanignancabinet_vousmaezriteznotreexpertise-vmne-oif-activity-7043590580588670976-Gm-L?utm_source=share&utm_medium=member_desktop';
     let baseUrlFacebook = 'https://www.facebook.com/lanignancabinet/posts/pfbid02uupCn91sxGEVioUWweXzVDpLJSLXy5oYAvJNXkRBYQTZwuRg4s8N5myaMFKds44Kl';
-    let title = document.querySelector('meta[property="og:title"]').getAttribute("content"); // Titre de l'article
-    let description = document.querySelector('meta[property="og:description"]').getAttribute("content"); // Description de l'article
-    let image = document.querySelector('meta[property="og:image"]').getAttribute("content"); // Image de l'article
+    let title = document.querySelector('meta[property="og:title"]').getAttribute("content");
+    let description = document.querySelector('meta[property="og:description"]').getAttribute("content");
+    let image = document.querySelector('meta[property="og:image"]').getAttribute("content");
   
     // Partager sur Twitter
     // var twitterUrl = 'https://twitter.com/share?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(title);
+
+    let button = document.querySelector("button");
+    button.addEventListener("click", () =>{
+        let link = window.location.href;
+        let root = window.location.origin;
+        let filteredUrl = link.replace(root, '');
+        console.log(filteredUrl);
+        // link.select();
+
+
+
+
+        navigator.clipboard.writeText(filteredUrl);
+    })
+
 function shareLinkedin() { 
-    // Partager sur LinkedIn
     let linkedinUrl = 'https://www.linkedin.com/shareArticle?url=' + encodeURIComponent(baseUrlLinkedin) + '&title=' + encodeURIComponent(title) + '&summary=' + encodeURIComponent(description) + '&source=' + encodeURIComponent(window.location.hostname);
 
    window.open(linkedinUrl, 'Partager sur LinkedIn', 'width=600,height=500');
