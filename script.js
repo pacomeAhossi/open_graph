7b609199b6b0dd5bc554a3a5c3d40a3b30a7e6ef
Pacome <a.pacome@lanignan.com>
2023-03-27 16:56:55 +0200

    Recadrage du pop-up au centre

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let baseUrlLinkedin = 'https://www.linkedin.com/posts/lanignancabinet_vousmaezriteznotreexpertise-vmne-oif-activity-7043590580588670976-Gm-L?utm_source=share&utm_medium=member_desktop';
-let baseUrlFacebook = 'https://www.facebook.com/lanignancabinet/posts/pfbid02uupCn91sxGEVioUWweXzVDpLJSLXy5oYAvJNXkRBYQTZwuRg4s8N5myaMFKds44Kl';
-let title = document.querySelector('meta[property="og:title"]').getAttribute("content");
-let description = document.querySelector('meta[property="og:description"]').getAttribute("content");
-let image = document.querySelector('meta[property="og:image"]').getAttribute("content");
+const width = 600,
+      height = 500,
+      left_position = (window.innerWidth - width) / 2,
+      top_position = (window.innerHeight - height) / 2;
 
-// Partager sur Twitter
-// var twitterUrl = 'https://twitter.com/share?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(title);
+let baseUrlLinkedin = 'https://www.linkedin.com/posts/lanignancabinet_vousmaezriteznotreexpertise-vmne-oif-activity-7043590580588670976-Gm-L?utm_source=share&utm_medium=member_desktop',
+    baseUrlFacebook = 'https://www.facebook.com/lanignancabinet/posts/pfbid02uupCn91sxGEVioUWweXzVDpLJSLXy5oYAvJNXkRBYQTZwuRg4s8N5myaMFKds44Kl',
+     title = document.querySelector('meta[property="og:title"]').getAttribute("content"),
+     description = document.querySelector('meta[property="og:description"]').getAttribute("content"),
+     image = document.querySelector('meta[property="og:image"]').getAttribute("content");
+
 
 let button = document.querySelector("button");
 let copyText = document.querySelector(".copy-txt");
+
 button.addEventListener("click", () => {
     let link = window.location.href;
     let root = window.location.origin;
     let filteredUrl = link.replace(root, '');
-
-
-
-
     navigator.clipboard.writeText(filteredUrl);
     copyText.classList.add("active");
     window.getSelection().removeAllRanges();
@@ -28,11 +28,11 @@ button.addEventListener("click", () => {
 function shareLinkedin() {
     let linkedinUrl = 'https://www.linkedin.com/shareArticle?url=' + encodeURIComponent(baseUrlLinkedin) + '&title=' + encodeURIComponent(title) + '&summary=' + encodeURIComponent(description) + '&source=' + encodeURIComponent(window.location.hostname);
 
-    window.open(linkedinUrl, 'Partager sur LinkedIn', 'width=600,height=500');
+    window.open(linkedinUrl, 'Partager sur LinkedIn', 'width=' + width + ',height=' + height + ',left=' + left_position + ',top=' + top_position);
 }
 
 function shareFacebook() {
     let facebookUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(baseUrlFacebook);
 
-    window.open(facebookUrl, 'Partager sur Facebook', 'width=600,height=500');
+    window.open(facebookUrl, 'Partager sur Facebook', 'width=' + width + ',height=' + height + ',left=' + left_position + ',top=' + top_position);
 }
