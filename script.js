29ee49fbeb3b7adbb342832ffd9e6b76c61476bf
Pacome <a.pacome@lanignan.com>
2023-03-30 14:23:28 +0200

    Changement de l'url par l'url de la page actuelle

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const width = 600,
       left_position = (window.innerWidth - width) / 2,
       top_position = (window.innerHeight - height) / 2;
 
-let baseUrlLinkedin = 'https://www.linkedin.com/posts/lanignancabinet_vousmaezriteznotreexpertise-vmne-oif-activity-7043590580588670976-Gm-L?utm_source=share&utm_medium=member_desktop',
-    baseUrlFacebook = 'https://www.facebook.com/lanignancabinet/posts/pfbid02uupCn91sxGEVioUWweXzVDpLJSLXy5oYAvJNXkRBYQTZwuRg4s8N5myaMFKds44Kl',
+let url = window.location.href;
      title = document.querySelector('meta[property="og:title"]').getAttribute("content"),
      description = document.querySelector('meta[property="og:description"]').getAttribute("content"),
      image = document.querySelector('meta[property="og:image"]').getAttribute("content");
@@ -15,8 +14,7 @@ let baseUrlLinkedin = 'https://www.linkedin.com/posts/lanignancabinet_vousmaezri
 let button = document.querySelector("button");
 let copyText = document.querySelector(".copy-txt");
 
-button.addEventListener("click", () => {
-    // 
+button.addEventListener("click", () => { 
     let link =  window.location.protocol + '//' + window.location.hostname + window.location.pathname;
     let root = window.location.origin;
     let filteredUrl = link.replace(root, '');
@@ -29,13 +27,13 @@ button.addEventListener("click", () => {
 })
 
 function shareLinkedin() {
-    let linkedinUrl = 'https://www.linkedin.com/shareArticle?url=' + encodeURIComponent(baseUrlLinkedin) + '&title=' + encodeURIComponent(title) + '&summary=' + encodeURIComponent(description) + '&source=' + encodeURIComponent(window.location.hostname);
+    let linkedinUrl = 'https://www.linkedin.com/shareArticle?url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '&summary=' + encodeURIComponent(description) + '&source=' + encodeURIComponent(window.location.hostname);
 
     window.open(linkedinUrl, 'Partager sur LinkedIn', 'width=' + width + ',height=' + height + ',left=' + left_position + ',top=' + top_position);
 }
 
 function shareFacebook() {
-    let facebookUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(baseUrlFacebook);
+    let facebookUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url);
 
     window.open(facebookUrl, 'Partager sur Facebook', 'width=' + width + ',height=' + height + ',left=' + left_position + ',top=' + top_position);
 }
